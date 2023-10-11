f7f7cdc4917f203f95077b9b5e8a1621ef3c044e
Fatih <fatiheee@icloud.com>
2023-10-11 12:00:00 +0300

    React Router added

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,41 @@
 import React from "react";
 import SearchBar from './SearchBar';
 import MovieList from './MovieList';
+import AddMovie from './AddMovie';
+import axios from "axios";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 
 class App extends React.Component {
 
     state = {
         movies: [],
-
         searchQuery: ""
-                  
-    }
 
-   async componentDidMount(){
-        const baseURL = " http://localhost:3002/movies";
-        const response = await fetch(baseURL);
-        const data = await response.json();
-        console.log(data);
-        this.setState({movies: data})
     }
 
 
-    deleteMovie = (movie) => {
+    async componentDidMount() {
+        const response = await axios.get("http://localhost:3002/movies")
+        this.setState({ movies: response.data })
+    }
+
+
+    deleteMovie = async (movie) => {
+        axios.delete(`http://localhost:3002/movies/${movie.id}`)
         const newMovieList = this.state.movies.filter(m => m.id !== movie.id);
-
         this.setState(state => ({
             movies: newMovieList
         }))
+    }
 
-   }
-
-   searchMovie = (event) => {
+    searchMovie = (event) => {
         this.setState({
             searchQuery: event.target.value
         })
-   }
+    }
 
-   render(){
+    render() {
 
         let filteredMovies = this.state.movies.filter(
             (movie) => {
@@ -44,25 +43,31 @@ class App extends React.Component {
             }
         )
 
-        return(
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-12">
-                        <SearchBar
-                        searchQueryProp={this.searchMovie}
-                         />
+        return (
+            <Router>
+                
+                <Routes>
+                <Route path="/" element={
+                  <>
+                  <div className="container">
+                    <div className="row">
+                      <div className="col-lg-12">
+                        <SearchBar searchMovieProp={this.searchMovie} />
+                      </div>
                     </div>
-                    
-                    <MovieList 
-                    movies={filteredMovies}
-                    deleteMovieProp={this.deleteMovie}
+                    <MovieList
+                      movies={filteredMovies}
+                      deleteMovieProp={this.deleteMovie}
                     />
-
-                </div>
-            </div>
-        )
-
-   }
+                    </div>
+                    </>
+                }> 
+                </Route>
+                <Route path="add" element={<AddMovie />}/>
+                </Routes>
+            </Router>
+          );
+    }
 }
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,23 +2,28 @@ import React from "react";
 
 class SearchBar extends React.Component {
 
-    
+
 
     handleFormSubmit = (event) => {
         event.preventDefault();
     }
 
 
-    render(){
+    render() {
 
-        return(
+        return (
             <form onSubmit={this.handleFormSubmit}>
-                <div className="form-row mb-5 mt-2">
-                    <div className="col-12">
-                        <input type="text" className="form-control" placeholder="Search a movie" 
-                        onChange={this.props.searchQueryProp}
-                        >                 
-                        </input>
+                <div className="row mb-5">
+                    <div className="col-10">
+                        <input type="text" className="form-control" placeholder="Search a movie"
+                            onChange={this.props.searchMovieProp}
+                        />
+                    </div>
+                    <div className="col-2">
+                        <button type="button"
+                            className="btn btn-md btn-danger"
+                            style={{ float: 'right' }}>Add Movie
+                        </button>
                     </div>
                 </div>
             </form>
